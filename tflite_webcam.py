4c94e38f474974429c40cad6bcb52d255d2768c2
Jim Jetsada Machom <jimjm@fb.com>
2020-03-29 15:54:12 +0100

    update

--- tflite_webcam.py
+++ tflite_webcam.py
@@ -51,7 +51,6 @@ class VideoStream:
         self.stopped = True
 
 
-global frame_counter
 frame_counter = 0
 
 
@@ -190,7 +189,7 @@ while True:
 
     # Loop over all detections and draw detection box if confidence is above minimum threshold
     for i in range(len(scores)):
-        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+        if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
             ymin = int(max(1, (boxes[i][0] * imH)))
