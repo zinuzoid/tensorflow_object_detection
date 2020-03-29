0d195d04027f69b5725e14b8e041000034eab231
Jim Jetsada Machom <jimjm@fb.com>
2020-03-30 00:55:39 +0100

    update

--- template_labelimg.py
+++ template_labelimg.py
@@ -1,7 +1,7 @@
 import os
 
-ANNOTATIONS_DIR = '/Users/jimjm/jim/workspace/tensorflow/training/annotations'
-TRAIN_DIR = '/Users/jimjm/jim/workspace/tensorflow/training/images/train'
+TRAIN_DIR = '/Users/jimjm/jim/workspace/tensorflow/training/images/test'
+ANNOTATIONS_DIR = TRAIN_DIR
 
 
 def get_filename(filepath):
@@ -9,19 +9,28 @@ def get_filename(filepath):
     return filename
 
 
+def get_fileext(filepath):
+    _, fileext = os.path.splitext(filepath)
+    return fileext
+
+
 def main():
     annotations = os.listdir(ANNOTATIONS_DIR)
+    annotations = list(filter(lambda s: get_fileext(s) == '.xml', annotations))
     annotations.sort()
+    print(annotations)
     last_annotation = list(annotations)[-1]
 
     images = os.listdir(TRAIN_DIR)
+    images = list(filter(lambda s: get_fileext(s) == '.jpg', images))
     images.sort()
+    print(images)
     last_image_filename = get_filename(last_annotation) + '.jpg'
     last_image_index = images.index(last_image_filename)
     next_image_filename = images[last_image_index + 1]
 
-    next_annotation_filename = get_filename(next_image_filename) + '.xml'
-
+    next_annotation_filename = get_filename(next_image_filename) + '.xml' \
+ \
     with open(ANNOTATIONS_DIR + '/' + last_annotation) as f:
         lines = f.readlines()
         if len(lines) == 0:
@@ -29,8 +38,6 @@ def main():
         lines[0] = '<annotation>'
         lines[2] = lines[2].replace(last_image_filename, next_image_filename)
         lines[3] = lines[3].replace(last_image_filename, next_image_filename)
-        print(lines[2])
-        print(lines[3])
 
     next_annotation_filepath = ANNOTATIONS_DIR + '/' + next_annotation_filename
     if os.path.exists(next_annotation_filepath):
@@ -39,10 +46,7 @@ def main():
     with open(next_annotation_filepath, 'w') as f:
         f.writelines(lines)
 
-    print(last_annotation)
     print(next_annotation_filename)
-
-    print(last_image_index)
     print(next_image_filename)
 
 
